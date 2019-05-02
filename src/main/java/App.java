33829baaacbde7654e89b660d077d2515b500985
wolf <billodida420@gmail.com>
2019-05-02 10:47:06 +0300

    coming up with endangered animals route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,6 +20,9 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+
+        // routing the homepage
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("animals", Animal.all());
@@ -29,5 +32,55 @@ public class App {
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-    }
+
+
+        // route to add new animal
+        get("/animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("animals", Animal.all());
+            model.put("endangeredAnimals", EndangeredAnimals.all());
+            model.put("template", "templates/animal-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        // route to add new ranger
+        get("/rangers/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/ranger_form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        // getting the endangered animals
+
+        get("/endangeredanimals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String health = request.params("health");
+            String age = request.params("age");
+            String name = request.params("name");
+            EndangeredAnimals endangeredAnimals = new EndangeredAnimals(name, health, age);
+            endangeredAnimals.save();
+            model.put("template", "templates/endangered-animals-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //checking if animal is normal or endangered
+        post("/animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String type = request.params("type");
+
+            if(type.equals("endangered"))
+            {
+                response.redirect("/endangeredanimals/new");
+            }
+            else {
+                String name = request.queryParams("name");
+                Animal Animal = new Animal(name);
+                Animal.save();
+                model.put("animals", Animal.all());
+                model.put("template", "templates/animal-success.vtl");
+            }
+                return new ModelAndView(model, layout);
+            }, new VelocityTemplateEngine());
+
 }
+    }
