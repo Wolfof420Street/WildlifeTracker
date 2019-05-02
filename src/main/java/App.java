bb8b886f4c98e97f4a3295d7edd73bf391cc4c44
wolf <billodida420@gmail.com>
2019-05-02 11:57:47 +0300

    adding rangers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -43,13 +43,25 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
         // route to add new ranger
         get("/rangers/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "templates/ranger_form.vtl");
+            String name = request.params("name");
+            String contactInfo = request.params("contactInfo");
+            Ranger ranger = new Ranger(name, contactInfo);
+            ranger.save();
+            model.put("template", "templates/ranger-form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        //getting new Rangers
+        get("/rangers", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("rangers", Ranger.all());
+        model.put("template", "templates/rangers.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
         // getting the endangered animals
 
         get("/endangeredanimals/new", (request, response) -> {
@@ -66,8 +78,9 @@ public class App {
         //checking if animal is normal or endangered
         post("/animal/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            String type = request.params("type");
+            String type = request.queryParams("type");
 
+            System.out.println(type);
             if(type.equals("endangered"))
             {
                 response.redirect("/endangeredanimals/new");
@@ -82,5 +95,26 @@ public class App {
                 return new ModelAndView(model, layout);
             }, new VelocityTemplateEngine());
 
-}
+        post("/endangeredanimals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String health = request.params("health");
+            String age = request.params("age");
+            String name = request.params("name");
+            EndangeredAnimals endangeredAnimals = new EndangeredAnimals(name, health, age);
+            endangeredAnimals.save();
+            model.put("endangeredAnimals", EndangeredAnimals.all());
+            model.put("template", "templates/endangered-animals-sucess.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        post("/rangers", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String contactInfo = request.queryParams("contactInfo");
+            Ranger newRanger = new Ranger(name, contactInfo);
+            newRanger.save();
+            model.put("template", "templates/stylist-success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
+}
