72dedc9242f51a6d8456d4e2672651c018f3b78d
wolf <billodida420@gmail.com>
2019-05-02 06:39:50 +0300

    adding the velocity template engine to begin routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,32 @@
-public class App {
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
+
+public class App {
+    public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("animals", Animal.all());
+            model.put("endangeredAnimals", EndangeredAnimals.all());
+            model.put("sighting", Sighting.all());
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+    }
 }
